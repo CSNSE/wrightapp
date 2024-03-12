dda90f21d8b455eea2d989ee866b7cb68d1d0bbd
Clement-Wright <clewri163@gmail.com>
2024-03-12 14:29:06 -0400

    https://github.com/CSNSE/wrightapp/issues/1#issue-2180489971

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -16,5 +16,20 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "function": {
+    "graphQlLambdaAuthorizer9855c633": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "veryfriendlybucket": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
